0b6d74e328be4945d3f815e74ac510a8bb9cb79e
ahmadowais-dev <ahmad.owais@intravar.com>
2024-08-07 10:47:06 +0500

    initial commit

---
+++ src/app/feed/page.js
@@ -0,0 +1,13 @@
+import {getPosts} from "@/src/lib/posts";
+import Posts from "@/src/components/posts";
+
+
+export default async function FeedPage() {
+  const posts = await getPosts();
+  return (
+    <>
+      <h1>All posts by all users</h1>
+      <Posts posts={posts} />
+    </>
+  );
+}
